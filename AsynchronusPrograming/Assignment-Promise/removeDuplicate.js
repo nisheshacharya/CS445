235cd11c6ce5449d365743cfe42912312e03ebea
nisheshacharya <116048513+nisheshacharya@users.noreply.github.com>
2023-05-18 00:02:33 -0500

    corrected second question

--- AsynchronusPrograming/Assignment-Promise/removeDuplicate.js
+++ AsynchronusPrograming/Assignment-Promise/removeDuplicate.js
@@ -1,33 +1,13 @@
-async function removeDuplicate(arr) {
 
-    for (let i = 0; i < arr.length; i++) {
-        for (let j = 0; j < arr.length; j++) {
-            if (arr[i] == arr[j] && i != j) {
-                arr.splice(i, 1);
-            }
-        }
-    }
-    
-    console.log("Inside await.")
-    return arr;
-}
+Array.prototype.removeDuplicatesAsync = function() {
+    return new Promise((resolve) => {
+        let unique = [...new Set(this)];
+        resolve(unique);
+    });
+};
 
+console.log("start");
+[1,1,2,2,3,3,4,4,5,5,6,6,7,8,9].removeDuplicatesAsync().then(console.log); 
+console.log("end");
 
-async function removeDupAwait(arr){
-    try{
-        console.log("before await")
-       let data = await(removeDuplicate(arr));
-       console.log(data);
-
-
-    }
-    catch{
-        console.log("Not able to run");
-    }
-    finally{
-        console.log("End of program")
-    }
-}
-
-removeDupAwait([1,2,2,3,3,4,5]);
 
